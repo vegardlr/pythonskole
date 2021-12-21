063e7b9ef157387df8bf31330e031ce2a719a692
Vegard Rekaa <vegard@rekaa.no>
2021-12-22 00:35:42 +0100

    Preparations for v1.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from pathlib import Path
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
-version = '1.0'
+version = '1.1'
 
 setup(
   name = 'pythonskole',         # How you named your package folder (MyLib)
