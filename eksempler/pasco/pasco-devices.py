e1e593117d666bdd0392e634a9ce4dcbe70d6399
Vegard Rekaa <vegard@rekaa.no>
2022-01-25 10:30:26 +0100

    Update pasco-devices.py

--- eksempler/pasco/pasco-devices.py
+++ eksempler/pasco/pasco-devices.py
@@ -2,53 +2,44 @@
 # Looks for Pasco devices and prints their available 
 # sensors and measurments. Then disconnects.
 # Written by Vegard Rekaa 2021, kontakt@pythonskole.no
+# Updated January 2022
 # 
-# Import pasco libraries
-# Documentation: https://pypi.org/project/pasco/
+# Documentation on pasco libraries: https://pypi.org/project/pasco/
 from sys import exit
 from pasco.pasco_ble_device import PASCOBLEDevice
-from pasco.code_node_device import CodeNodeDevice
-from pasco.character_library import Icons
+#from pasco.code_node_device import CodeNodeDevice
+#from pasco.character_library import Icons
 
-# Create handle to manage devices
+# Create a code element, through which we can handle the instrument
 device = PASCOBLEDevice()
+# List all devices
+device_list = device.scan()
+if len(device_list) == 0:
+    #Leace program if no devices are found
+    print("No device found")
+    exit(0)
 
-# Read available bluetooth devics. 
-# Please make sure of the following 
-# before running this code: 
-#  - the PC's bluetooth is on
-#  - the device(s) is on and blinking red (not green)
-#  - the device(s) are close to the PC
-found_devices = device.scan()
-n = len(found_devices)
-print("Found ",n," devices")
-if n == 0: exit(0)  #Exit code if no devices are found
+# Print the list of devices
+for i, dev in enumerate(device_list):
+    print(str(i)+":"+str(dev))
+# Get user input which device you want. 
+# If there is only one, select that device automatically
+select = input('Select a device: ') if len(device_list) > 1 else 0
+select_device = device_list[int(select)]
+#Print the name and ID of the selected device, and connect
+print("Connecting to:"+str(select_device))
+device.connect(select_device)
 
-#Print list of available devices
-for i, ble_device in enumerate(found_devices):
-    print(f'{i}: {ble_device.name}')
-
-#Ask for user input, for what device we want to analyze
-selected_device = found_devices[int(input('Select a device: '))]
-
-#Connect to device
-print("Connecting to:"+str(selected_device))
-device.connect(selected_device)
-
-#Print warning if connection failed, exit code if fail
+#Leave code if connection failed
 if not device.is_connected():
     print("Connection failed")
     exit(0)
 
-#Print list of sensors on device
+#Print list of sensors on connected device
 print("Sensor list:", device.get_sensor_list())
-#Print list of measurments available with the sensors
+#Print list of measurments available from the connected device
 print("Measurement list:", device.get_measurement_list())
 
-#Clean disconnect. If the code exists with an error between 
-#device.connect() and device.disconnect(), the devices will blink
-#green after running this code. To make them available 
-#for a new connection, you need to first turn them off, then 
-#restart them (you want a red blinking light) on the bluetooth-lamp
+#Clean disconnect. 
 device.disconnect()
 
