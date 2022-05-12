59f573217c08485b48c4d88de7f941f7539abadb
Vegard Rekaa <vegard@rekaa.no>
2022-05-12 21:30:57 +0200

    Various fixes

--- eksempler/astronomi/fotometri/mizarobs/viewdata.py
+++ eksempler/astronomi/fotometri/mizarobs/viewdata.py
@@ -4,21 +4,21 @@ import numpy as np
 from astropy.time import Time
 import matplotlib.pyplot as plt
 
-
+#Åpne mappe og finn datafil, les til minnet. 
 if len(sys.argv) < 2:
-    print("Usage: getdata.py datafolder")
+    print("Usage: viewdata.py datafolder")
     sys.exit(1)
-
 datafolder = sys.argv[1]
 datafile = datafolder+"/data.txt"
-
 f = open(datafile,'r')
 data = np.genfromtxt(f,delimiter='\t')
 
+#Lagre relevante kolonner fra datafilen som tid og mag
 tid = Time(data[:,0],format='jd').to_value('datetime64')
 mag = data[:,1]
 
-plt.plot(tid,mag,'.')
+#Vis data - Resten av jobben gjøres i plottevinduet eller "på ark"
+plt.plot(tid,mag,'-.')
 plt.grid(True)
 plt.xlabel("Tid")
 plt.ylabel("Mag")
@@ -26,23 +26,3 @@ plt.title(datafolder)
 ax = plt.gca()
 ax.invert_yaxis()
 plt.show()
-
-hours = np.timedelta64(tid[1]-tid[0],'h').astype(float)
-seconds = np.timedelta64(tid[1]-tid[0],'s').astype(float)
-
-#N    = len(mag)
-#tid_sec = #TODO gjør om tid til en array med sekunder siden start
-#tid2 = np.linspace(seconds,len(mag))
-#mag2 = np.interp(tid2,tid,mag)
-
-
-#plt.plot(tid2,mag2,'-')
-#plt.show()
-
-
-#ps = np.abs(np.fft.fft(mag))**2
-#time_step = 1 / 30
-#freqs = np.fft.fftfreq(data.size, time_step)
-#idx = np.argsort(freqs)
-#plt.plot(ps)
-#plt.show()
