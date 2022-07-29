0972ff2ce2cf6b48aa50b59b0cca8dc0eaf9c926
Vegard Rekaa <vegard@rekaa.no>
2022-07-29 09:52:42 +0200

    Updates to setup.py and v1.3

--- setup.py
+++ setup.py
@@ -1,22 +1,25 @@
-
-from distutils.core import setup
+#TODO: Implement src-structure from https://www.freecodecamp.org/news/how-to-create-and-upload-your-first-python-package-to-pypi/
+#from distutils.core import setup
+import setuptools
 from pathlib import Path
-this_directory = Path(__file__).parent
+
+#this_directory = Path(__file__).parent
+version = '1.3'
 long_description = (this_directory / "README.md").read_text()
+with open("README.md", "r", encoding = "utf-8") as fh:
+    long_description = fh.read()
 
-version = '1.2'
-
-setup(
-  name = 'pythonskole',         # How you named your package folder (MyLib)
-  packages = ['pythonskole'],   # Chose the same as "name"
+setuptools.setup(
+  name              = 'pythonskole',     # How you named your package folder (MyLib)
+  packages          = ['pythonskole'],   # Chose the same as "name"
+  author            = 'Vegard Lundby Rekaa',        # Type in your name
+  author_email      = 'vegard@pythonskole.no',      # Type in your E-Mail
+  description       = 'Koder til bruk i undervisning i Python',   # Give a short description about your library
+  long_description  = long_description,
+  long_description_content_type = 'text/markdown',
+  url               = 'https://github.com/vegardlr/pythonskole',   # Provide either the link to your github or to your website
   version = version,      # Start with a small number and increase it with every change you make
   license='gpl-3.0',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'Koder til bruk i undervisning i Python',   # Give a short description about your library
-  long_description=long_description,
-  long_description_content_type='text/markdown',
-  author = 'Vegard Lundby Rekaa',                   # Type in your name
-  author_email = 'vegard@pythonskole.no',      # Type in your E-Mail
-  url = 'https://github.com/vegardlr/pythonskole',   # Provide either the link to your github or to your website
   download_url = 'https://github.com/vegardlr/pythonskole/archive/refs/tags/v'+version+'.tar.gz',    # I explain this later on
   keywords = ['Pythonskole', 'Undervisning', 'Koding','Python','Programmering','Læring'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
@@ -34,5 +37,7 @@ setup(
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10',
   ],
 )
