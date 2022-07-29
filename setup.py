ee0de01fd399fdf8128a16dda7818b3e260b4420
Vegard Rekaa <vegard@rekaa.no>
2022-07-29 10:23:03 +0200

    Update to version 1.0.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 #this_directory = Path(__file__).parent
 #long_description = (this_directory / "README.md").read_text()
-version = '1.0'
+version = '1.0.0'
 with open("README.md", "r", encoding = "utf-8") as fh:
     long_description = fh.read()
 
